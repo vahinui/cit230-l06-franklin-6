225aaf6f1ccf609d9dfdf42057db88e338716007
vahinui <38748398+vahinui@users.noreply.github.com>
2018-06-02 00:13:40 -1000

    0
    0

--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -1,24 +1,13 @@
 var d = new Date();
 
-var day = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var numberdate = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
 
-var month = new Array(12);
-month(0) = "January";
-month(1) =  "Febuary";
-month(2) = "March";
-month(3) = "April";
-month(4) = "May";
-month(5) = "June";
-month(6) = "July";
-month(7) = "August";
-month(8) = "Spetember";
-month(9) = "October";
-month(10) = "November";
-month(11) = "December";
-    
-    
-var currentDate= day[date.getDay()];
-var currentMonth = month[date.getMonth()];
+var month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "Spetember", "October", "November", "December"];
+
+var x1 = day[date.getDay()];
+var x2 = numberdate[date.getDate()];
+var x3 = month[date.getMonth]
 
 
-document.getElementById("currentdate").innerHTML = currentDate + "" + currentMonth "" + date.getFullYear();
+document.getElementById("currentdate").innerHTML = x1 + "," + x2 " " + x3 + " " + date.getFullYear();
