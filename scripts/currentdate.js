32ec8342033ebbea35425d48d59c8ddf35ab0dd9
vahinui <38748398+vahinui@users.noreply.github.com>
2018-06-08 11:08:26 -1000

    save

--- scripts/currentdate.js
+++ scripts/currentdate.js
@@ -1,13 +1,11 @@
-var d = new Date();
-
-var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var numberdate = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
-
-var month = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "Spetember", "October", "November", "December"];
-
-var x1 = day[date.getDay()];
-var x2 = numberdate[date.getDate()];
-var x3 = month[date.getMonth]
+var date = new Date();
+var weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var weekDate = weekday[date.getDay()];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "november", "December"];
+var day = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+//var currentdays = cdays[cdays.getDay()];
+var currentMonth = month[date.getMonth()];
+var currentDate = day[date.getDate()];
 
 
-document.getElementById("currentdate").innerHTML = x1 + "," + x2 " " + x3 + " " + date.getFullYear();
+document.getElementById("currentDate").innerHTML = weekDate + ", " + currentDate + " " + currentMonth + " " + date.getFullYear();
